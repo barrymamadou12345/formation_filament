b5db2210fdec24249545179f4904152704581d64
barrymamadou12345 <dioungol5432@gmail.com>
2025-02-21 16:45:32 +0000

    first commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => "Admin",
+            'email' => "admin@admin.com",
+            'telephone' => "7777777777",
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345'), // password
+            'remember_token' => Str::random(60),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return $this
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
